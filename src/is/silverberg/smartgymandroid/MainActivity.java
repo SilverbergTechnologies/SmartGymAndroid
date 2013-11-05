192d6c7e07d6473b0da6b95d106f44df091aebc8
Kjartan <kbk2@hi.is>
2013-11-05 23:10:04 +0000

    Add LFOpen and fix MainActivity

--- src/is/silverberg/smartgymandroid/MainActivity.java
+++ src/is/silverberg/smartgymandroid/MainActivity.java
@@ -15,14 +15,14 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
     
